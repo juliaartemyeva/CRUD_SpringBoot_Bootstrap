3e5417c8da66ec3a62c285d10bac49bc17c75250
juliaartemyeva <yulia.artemyeva91@gmail.com>
2020-03-13 22:46:48 +0300

    removed MVC-configuration

--- src/main/java/ru/task/crudapponspringboot/config/SecurityConfig.java
+++ src/main/java/ru/task/crudapponspringboot/config/SecurityConfig.java
@@ -11,16 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private UserDetailsService userDetailsService;
-
     @Autowired
     public SecurityConfig(@Qualifier("userDetailServiceImpl") UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
@@ -33,39 +30,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
-        http.addFilterBefore(filter, CsrfFilter.class);
-        http.formLogin()
-                // указываем страницу с формой логина
+        http
+                .formLogin()
                 .loginPage("/login")
-                //указываем логику обработки при логине
-                //     .successHandler(new LoginSuccessHandler())
-                // указываем action с формы логина
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("/login")
-                // Указываем параметры логина и пароля с формы логина
-//                .usernameParameter("username")
-//                .passwordParameter("password")
-                // даем доступ к форме логина всем
+                .usernameParameter("username")
+                .passwordParameter("password")
                 .permitAll();
-        http.logout()
-                // разрешаем делать логаут всем
-                .permitAll()
-                // указываем URL логаута
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                // указываем URL при удачном логауте
-                .logoutSuccessUrl("/login")
-                //выклчаем кроссдоменную секьюрность (на этапе обучения неважна)
-                .and().csrf().disable();
-
         http
-                // делаем страницу регистрации недоступной для авторизированных пользователей
+                .logout()
+                .permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .and().csrf().disable();
+        http
                 .authorizeRequests()
-                .antMatchers("/static/**", "/").permitAll()
-                .antMatchers("/admin**").hasAuthority("ADMIN")
-                .antMatchers("/user").hasAnyAuthority("USER", "ADMIN");
+                .antMatchers( "/user").hasAnyAuthority("USER", "ADMIN")
+                .antMatchers("/admin**").hasAuthority("ADMIN");
     }
 
     @Bean
@@ -73,4 +55,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return NoOpPasswordEncoder.getInstance();
     }
 }
+
+
 
